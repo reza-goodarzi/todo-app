ff064cd36b8a9d3e88238cf193e1ad8066bd767a
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-06-09 03:45:09 +0430

    Add main layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,34 @@
-import { ThemeProvider } from "@mui/material";
-import theme from "../styles/theme";
-import { AppBar } from "@mui/material";
-import styled from "@emotion/styled";
+import { Box, Typography, AppBar, Button } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import Link from "next/link";
 
 function Layout({ children }) {
   return (
     <>
-      <AppBarStyled>NAVBAR</AppBarStyled>
+      <AppBar
+        position="static"
+        sx={{
+          p: "20px",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <AccountCircle sx={{ fontSize: 32 }} />
+          <Typography>Reze Goodazi</Typography>
+        </Box>
+
+        <Button sx={{ color: "inherit" }}>Logout</Button>
+
+        {/* <Box>
+          <Button sx={{ color: "inherit" }}>Login</Button>
+          <Button sx={{ color: "inherit" }}>Sign In</Button>
+        </Box> */}
+      </AppBar>
       <main>{children}</main>
     </>
   );
 }
 
 export default Layout;
-
-const AppBarStyled = styled(AppBar)`
-  padding: 20px;
-`;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -10,16 +10,12 @@ const theme = createTheme({
       white: "#f8f8f8",
     },
     primary: {
-      light: "#60d48750",
       main: "#28aa6c",
-      dark: "#006f4f",
       contrastText: "#f8f8f8",
     },
 
     secondary: {
-      light: "#83deec80",
       main: "#34c7dc",
-      dark: "#1c97a8",
       contrastText: "#f8f8f8",
     },
     background: {
