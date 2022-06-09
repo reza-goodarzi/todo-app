1b91203b2e8738e7320576e9b56b98cce55f4928
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-06-09 12:14:33 +0430

    compelete signup Functionality

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,79 +1,167 @@
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Backdrop, Button, CircularProgress, Grid, TextField, Typography } from "@mui/material";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSignup } from "../store/user-actions";
 
 function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [age, setAge] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const dispatch = useDispatch();
+  const { isToggle, error, token } = useSelector((state) => state.user);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Login");
+
+    if (password !== confirmPassword) {
+      setErrorMessage("Password do not match!");
+      return;
+    }
+
+    if (age > 99 || age < 1) {
+      setErrorMessage("Age is not valid!");
+      return;
+    }
+
+    setLoading(true);
+    setErrorMessage("");
+
+    dispatch(fetchSignup({ name, email, password, age }));
+  };
+
+  useEffect(() => {
+    setLoading(false);
+
+    if (error) {
+      setErrorMessage(error);
+    }
+  }, [isToggle, error]);
+
+  useEffect(() => {
+    if (token) {
+      router.push("/");
+      setLoading(true);
+    }
+  }, [token, router]);
+
   return (
-    <form>
-      <Typography
-        color="primary"
-        variant="h1"
-        sx={{ fontWeight: "bold", textAlign: "center", m: 5 }}
-      >
-        SIGNUP
-      </Typography>
-      <Grid
-        container
-        spacing={4}
-        direction="column"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Grid item sx={{ width: 400 }}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            type="text"
-            label="Full Name"
-            placeholder="Please enter your full name"
-          />
-        </Grid>
+    <>
+      <form onSubmit={handleSubmit}>
+        <Typography
+          color="primary"
+          variant="h1"
+          sx={{ fontWeight: "bold", textAlign: "center", m: 5 }}
+        >
+          SIGNUP
+        </Typography>
+        <Grid
+          container
+          spacing={4}
+          direction="column"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Grid item sx={{ width: 400 }}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              type="text"
+              label="Full Name"
+              placeholder="Please enter your full name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </Grid>
 
-        <Grid item sx={{ width: 400 }}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            type="email"
-            label="Email"
-            placeholder="Please enter your email"
-          />
-        </Grid>
+          <Grid item sx={{ width: 400 }}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              type="email"
+              label="Email"
+              placeholder="Please enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Grid>
 
-        <Grid item sx={{ width: 400 }}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            type="password"
-            label="Password"
-            placeholder="Please enter your password"
-          />
-        </Grid>
+          <Grid item sx={{ width: 400 }}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              type="password"
+              label="Password"
+              placeholder="Please enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Grid>
 
-        <Grid item sx={{ width: 400 }}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            type="number"
-            label="Age"
-            placeholder="Please enter your age"
-          />
-        </Grid>
+          <Grid item sx={{ width: 400 }}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              type="password"
+              label="Confirm Password"
+              placeholder="Please confirm your password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </Grid>
 
-        <Grid item sx={{ width: 400 }}>
-          <Button fullWidth variant="contained" sx={{ color: "#f8f8f8", p: 2 }} type="submit">
-            Signup
-          </Button>
-          <Typography sx={{ mt: 1.5, textAlign: "center" }}>
-            do you have an account?
-            <Link href="/signup">
-              <Button sx={{ p: 0, textTransform: "capitalize" }}>Login</Button>
-            </Link>
-          </Typography>
+          <Grid item sx={{ width: 400 }}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              type="number"
+              label="Age"
+              placeholder="Please enter your age"
+              value={age}
+              onChange={(e) => setAge(Number(e.target.value))}
+              required
+            />
+          </Grid>
+
+          <Grid item sx={{ width: 400 }}>
+            {errorMessage && (
+              <Typography color="error" sx={{ mb: 1, fontSize: 14 }}>
+                {errorMessage}
+              </Typography>
+            )}
+            <Button fullWidth variant="contained" sx={{ color: "#f8f8f8", p: 2 }} type="submit">
+              Signup
+            </Button>
+            <Typography sx={{ mt: 1.5, textAlign: "center" }}>
+              do you have an account?
+              <Link href="/signup">
+                <Button sx={{ p: 0, textTransform: "capitalize" }}>Login</Button>
+              </Link>
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-    </form>
+      </form>
+
+      <Backdrop open={loading}>
+        <CircularProgress size="32px" />
+      </Backdrop>
+    </>
   );
 }
 
--- src/store/user-actions.js
+++ src/store/user-actions.js
@@ -28,7 +28,48 @@ export const fetchLogin = ({ email, password }) => {
         })
       );
     } catch (error) {
-      console.log(error.message);
+      dispatch(userActions.getError(error.message));
+    }
+  };
+};
+
+export const fetchSignup = ({ name, email, password, age }) => {
+  return async (dispatch) => {
+    try {
+      const res = await fetch(`${API_URL}/users`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+          age,
+        }),
+      });
+
+      const data = await res.json();
+      "".startsWith();
+      if (!res.ok) {
+        if (
+          data.error.startsWith(
+            "E11000 duplicate key error collection: todo.users index: email_1 dup key:"
+          )
+        ) {
+          throw new Error("Already exists an account registered with this email address.");
+        } else {
+          throw new Error(data.error);
+        }
+      }
+
+      dispatch(
+        userActions.addEnteredUser({
+          user: data.user,
+          token: data.token,
+        })
+      );
+    } catch (error) {
       dispatch(userActions.getError(error.message));
     }
   };
