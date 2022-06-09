8e0f1c219f8f911b34be8f9a5125f3b2940c3311
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-06-09 04:37:26 +0430

    Build ui of the signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,80 @@
-import React from "react";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import Link from "next/link";
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <form>
+      <Typography
+        color="primary"
+        variant="h1"
+        sx={{ fontWeight: "bold", textAlign: "center", m: 5 }}
+      >
+        SIGNUP
+      </Typography>
+      <Grid
+        container
+        spacing={4}
+        direction="column"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Grid item sx={{ width: 400 }}>
+          <TextField
+            fullWidth
+            variant="outlined"
+            type="text"
+            label="Full Name"
+            placeholder="Please enter your full name"
+          />
+        </Grid>
+
+        <Grid item sx={{ width: 400 }}>
+          <TextField
+            fullWidth
+            variant="outlined"
+            type="email"
+            label="Email"
+            placeholder="Please enter your email"
+          />
+        </Grid>
+
+        <Grid item sx={{ width: 400 }}>
+          <TextField
+            fullWidth
+            variant="outlined"
+            type="password"
+            label="Password"
+            placeholder="Please enter your password"
+          />
+        </Grid>
+
+        <Grid item sx={{ width: 400 }}>
+          <TextField
+            fullWidth
+            variant="outlined"
+            type="number"
+            label="Age"
+            placeholder="Please enter your age"
+          />
+        </Grid>
+
+        <Grid item sx={{ width: 400 }}>
+          <Button fullWidth variant="contained" sx={{ color: "#f8f8f8", p: 2 }} type="submit">
+            Signup
+          </Button>
+          <Typography sx={{ mt: 1.5, textAlign: "center" }}>
+            do you have an account?
+            <Link href="/signup">
+              <Button sx={{ p: 0, textTransform: "capitalize" }}>Login</Button>
+            </Link>
+          </Typography>
+        </Grid>
+      </Grid>
+    </form>
+  );
 }
 
 export default Signup;
